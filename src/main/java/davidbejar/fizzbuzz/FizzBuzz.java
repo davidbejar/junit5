a086779e8e581920540cf14b628ee87b08b706d4
davidbejar <david.bejar@gmail.com>
2016-11-27 09:30:14 +0700

    Extract common Logic.
    Reduce code duplication. Eases code readablity.

--- src/main/java/davidbejar/fizzbuzz/FizzBuzz.java
+++ src/main/java/davidbejar/fizzbuzz/FizzBuzz.java
@@ -13,21 +13,25 @@ import java.util.List;
  */
 public class FizzBuzz {
 
+	private static String fizzBuzzLogic(int i) {
+		if (i % 15 == 0) {
+			return FIZZBUZZ.value();
+		} else if (i % 5 == 0) {
+			return BUZZ.value();
+		} else if (i % 3 == 0) {
+			return FIZZ.value();
+		}
+		return String.valueOf(i);
+	}
+
 	public static List<String> fizzBuzzQuickWin() throws InterruptedException {
 		List<String> output = new ArrayList<>(100);
 		for (int i = 1; i <= 100; i++) {
-			if (i % 15 == 0) {
-				output.add(FIZZBUZZ.value());
-			} else if (i % 5 == 0) {
-				output.add(BUZZ.value());
-			} else if (i % 3 == 0) {
-				output.add(FIZZ.value());
-			} else {
-				output.add(String.valueOf(i));
-			}
+			output.add(fizzBuzzLogic(i));
 		}
 		Thread.sleep(10);// To help with benchmarking
 		return output;
+
 	}
 
 	public static List<String> fizzBuzzRecursive(List<String> fizzBuzzList) throws InterruptedException {
@@ -35,15 +39,7 @@ public class FizzBuzz {
 		if (i == 100 + 1) {
 			return fizzBuzzList;
 		} else {
-			if (i % 15 == 0) {
-				fizzBuzzList.add(FIZZBUZZ.value());
-			} else if (i % 5 == 0) {
-				fizzBuzzList.add(BUZZ.value());
-			} else if (i % 3 == 0) {
-				fizzBuzzList.add(FIZZ.value());
-			} else {
-				fizzBuzzList.add(String.valueOf(i));
-			}
+			fizzBuzzList.add(fizzBuzzLogic(i));
 			Thread.sleep(10);// To help with benchmarking
 			return fizzBuzzRecursive(fizzBuzzList);
 		}
